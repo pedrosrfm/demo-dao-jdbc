c87c1775b56ed159447a5f0f958ed9dace21539d
Pedro <pedrotaraves@gmail.com>
2023-09-06 14:59:15 -0300

    insert implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -29,5 +29,10 @@ public class Main {
         for (Seller seller1 : list){
             System.out.println(seller1);
         }
+
+        System.out.println("\n===TEST 4: seller insert===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
     }
 }
