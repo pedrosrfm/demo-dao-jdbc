ad5337a873d9b371aafc5cd16351f46bc9672c61
Pedro <pedrotaraves@gmail.com>
2023-09-08 08:06:40 -0300

    deleteById implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,9 +7,12 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         SellerDao sellerDao = DaoFactory.creatSellerDao();
 
@@ -40,5 +43,13 @@ public class Main {
         seller.setName("Martha Waine");
         sellerDao.update(seller);
         System.out.println("Update complete");
+
+        System.out.println("\n===TEST 6: seller deleteById===");
+        System.out.print("Enter id for delete test: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Seller deleted");
+
+        sc.close();
     }
 }
